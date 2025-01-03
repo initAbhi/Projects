c3a5453b2306686665322de7cbd3e99096d4ada2
Phantom <developerabhi17@gmail.com>
2025-01-03 22:16:32 +0530

    salting

--- initApp Store/backend/config/db.js
+++ initApp Store/backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log(`successfull connected to database 👍`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
--- initApp Store/backend/controllers/userController.js
+++ initApp Store/backend/controllers/userController.js
@@ -1,11 +1,33 @@
-import User from '../models/userModel.js';
-import asyncHandler from '../middlewares/asyncHandler.js';
+import User from "../models/userModel.js";
+import asyncHandler from "../middlewares/asyncHandler.js";
+import bcrypt from "bcryptjs"
 
-const createUser = asyncHandler((req, res) => {
-    console.log(req.body.username);
-    console.log(req.body.email);
-    console.log(req.body.password);
-    res.send("Done");
-})
+const createUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    throw new Error("Bro what the heck are you doing, Fill all the inputs");
+  }
+  let userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400).send("user already exists");
+  }else{
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt);
+      const newUser = new User({ username, email, password: hashedPassword });
+      try {
+          await newUser.save();
+          res.status(201).json({
+              _id: newUser._id,
+              username: newUser.username,   
+              email: newUser.email,
+              isAdmin: newUser.isAdmin,
+            });
+        } catch (error) {
+            res.status(400);
+            throw new Error("invalid user data :", error);
+        } 
+        res.send("Done");
+    }
+});
 
-export {createUser};
+export { createUser };
