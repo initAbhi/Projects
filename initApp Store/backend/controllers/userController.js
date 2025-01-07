7a56b5b2b159c3ce7936c42d90e6a071069032fd
Phantom <developerabhi17@gmail.com>
2025-01-07 22:23:36 +0530

    logout

--- initApp Store/backend/controllers/userController.js
+++ initApp Store/backend/controllers/userController.js
@@ -1,7 +1,7 @@
 import User from "../models/userModel.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
-import bcrypt from "bcryptjs"
-import createToken from "../utils/createToken.js"
+import bcrypt from "bcryptjs";
+import createToken from "../utils/createToken.js";
 
 const createUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
@@ -14,48 +14,53 @@ const createUser = asyncHandler(async (req, res) => {
   } else {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
-      const newUser = new User({ username, email, password: hashedPassword });
-      try {
-          await newUser.save();
-          createToken(res, newUser._id);
-          res.status(201).json({
-              _id: newUser._id,
-              username: newUser.username,   
-              email: newUser.email,
-              isAdmin: newUser.isAdmin,
-            });
-        } catch (error) {
-            res.status(400);
-            throw new Error("invalid user data :", error);
-        } 
-        // res.send("Done");
+    const newUser = new User({ username, email, password: hashedPassword });
+    try {
+      await newUser.save();
+      createToken(res, newUser._id);
+      res.status(201).json({
+        _id: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+        isAdmin: newUser.isAdmin,
+      });
+    } catch (error) {
+      res.status(400);
+      throw new Error("invalid user data :", error);
     }
     // res.send("Done");
   }
-);
+  // res.send("Done");
+});
 
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
-  if(!email || !password){
+  if (!email || !password) {
     throw new Error("Please fill all the inputs");
   }
+  ``;
   const user = await User.findOne({ email });
-  if(!user){
+  if (!user) {
     res.status(400).send("Invalid email or password");
-    }
-    const isMatch = await bcrypt.compare(password, user.password);
-    if(!isMatch){
-      res.status(400).send("Invalid email or password");
-    }
-    createToken(res, user._id);
-    res.status(200).json({
-      _id: user._id,
-      username: user.username,
-      email: user.email,
-      isAdmin: user.isAdmin,
-      });
-      
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    res.status(400).send("Invalid email or password");
+  }
+  createToken(res, user._id);
+  res.status(200).json({
+    _id: user._id,
+    username: user.username,
+    email: user.email,
+    isAdmin: user.isAdmin,
+  });
+  return;
+});
 
-})
+const logoutUser = asyncHandler(async (req, res) => {
+  res.clearCookie('jwt');
+  res.status(200).json({ message: "Logout sucessfully" });
 
-export { createUser, loginUser };
+});
+
+export { createUser, loginUser, logoutUser };
--- initApp Store/backend/routes/userRoutes.js
+++ initApp Store/backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 // import { createUser } from "../controllers/userController";
-import { createUser, loginUser } from "../controllers/userController.js";
+import { createUser, loginUser,logoutUser } from "../controllers/userController.js";
 const router = express.Router();
 
 router.route("/").post(createUser);
-router.post("/auth", loginUser);
+router.route("/auth").post(loginUser);
+router.route("/logout").post(logoutUser)
 
 export default router;
